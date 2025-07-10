58795e30281cd437b9f402c0700584f15958d928
wookie1234 <51341590+wookie1234@users.noreply.github.com>
2025-07-10 22:03:08 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EventApp from "./EventApp";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <EventApp />
+  </React.StrictMode>
+);
